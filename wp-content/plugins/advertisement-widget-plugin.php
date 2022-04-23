03cd50b5b1ec405b45f45a4c2239a093b31b34bd
Ksenia <200.ksenia.200@gmail.com>
2022-04-23 14:53:32 +0200

    add

---
+++ wp-content/plugins/advertisement-widget-plugin.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Plugin Name: Advertisement Widget Plugin
+Plugin URI: http://ex.com
+Description: Simple Advertisement Widget Plugin including banner image and link
+Version: 1.0
+Author: MAI
+Author URI: http://ex.com
+License: GPL2
+ */
+// The widget class
+
+class Advertisement_Widget extends WP_Widget
+{
+    // Main constructor
+    public function __construct()
+    {
+        $widget_ops = array('classname' => 'Advertisement_Widget', 'description' => 'Displays Ads');
+        $this->WP_Widget('Advertisement_Widget', 'Advertisement Widget', $widget_ops);
+    }
+
+    public function form($instance)
+    {
+        $instance = wp_parse_args((array) $instance, array('title' => ''));
+        $title = $instance['title'];
+        $src = esc_attr($instance['src']);
+        $link = esc_attr($instance['link']);
+        ?>
+<p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat"
+            id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>"
+            type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+<p><label for="<?php echo $this->get_field_id('src'); ?>"><?php _e('Advertisement Banner');?></label>
+    <textarea rows="4" cols="50" class="widefat" id="<?php echo $this->get_field_id('src'); ?>"
+        name="<?php echo $this->get_field_name('src'); ?>"><?php echo ($src); ?> </textarea>
+</p>
+<label for="<?php echo $this->get_field_id('link'); ?>"><?php _e('Ads Link');?></label>
+<input class="widefat" id="<?php echo $this->get_field_id('link'); ?>"
+    name="<?php echo $this->get_field_name('link'); ?>" type="text" value="<?php echo $link; ?>" />
+</p>
+<?php
+}
+
+public function update($new_instance, $old_instance)
+{
+        $instance = $old_instance;
+        $instance['title'] = $new_instance['title'];
+        $instance['src'] = $new_instance['src'];
+        $instance['link'] = $new_instance['link'];
+        return $instance;
+}
+
+public function widget($args, $instance)
+{
+        extract($args);
+        $title = apply_filters('widget_title', $instance['title']);
+        $src = $instance['src'];
+        $link = $instance['link'];
+?>
+<?php echo $before_widget; ?>
+<?php if ($title) {
+            echo $before_title . $title . $after_title;
+}
+?>
+<?php echo '<a href="' . $link . '" target="_blank"><img src=' . $src. " '/>"; ?></a>
+<?php echo $after_widget; ?>
+<?php
+}
+}
+
+// Register the widget
+function my_register_custom_widget()
+{
+    register_widget('Advertisement_Widget');
+}
+add_action('widgets_init', 'my_register_custom_widget');
--- wp-content/plugins/oad-more-scroll/load-more-scroll.php
+++ wp-content/plugins/oad-more-scroll/load-more-scroll.php
@@ -39,7 +39,7 @@ add_action('get_footer', 'create_script');
 function bootkit_load_posts()
 {
     $args = unserialize(stripslashes($_POST['query'])); // запрос от скрипта
-    $args['paged'] = $_POST['page']; // номер страницы
+    $args['paged'] = $_POST['page']+1; // номер страницы
     $args['post_status'] = 'publish'; // только опубликованные
     $args['posts_per_page'] = get_option( 'posts_per_page' ); // сколько постов на странице (подгружать по 2)
     // определяет, какие посты будут показаны в базовом цикле WordPress
